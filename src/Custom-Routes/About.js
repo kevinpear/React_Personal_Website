c61ff20e420f57092037ddb44c35bcb00c7153e6
Kevin Li <kli295@wisc.edu>
2023-07-11 13:49:03 -0500

    Added Logo Story

--- src/Custom-Routes/About.js
+++ src/Custom-Routes/About.js
@@ -12,12 +12,18 @@ const About = () => {
             <p> 
               This is a react website hosted on AWS Amplify. 
               The hosting setup was created by following a <a href="https://aws.amazon.com/getting-started/hands-on/build-react-app-amplify-graphql/">AWS guide</a>.
-              Everything else was built from scratch. You can see the source code at <a href="https://github.com/kevinpear/React_Personal_Website">Github</a>.
+              Everything else was built from scratch. You can see the source code at <a href="https://github.com/kevinpear/React_Personal_Website">Github</a>.               
+            </p>
+            <p>
+            The logo features a pear because, as a child, I mistakenly thought that my last name in Chinese, "Li" or "李," translated to "Pear". 
+              (The correct translation is "Plum"; "Pear" is "梨"). 
+              This led me to create multiple online profiles with pear-themed usernames. 
+              Even after learning the correct translation, I grew fond of the logo and decided to keep it.
             </p>
             <h2 className="Title">Contact Information:</h2>
             <p>Email: kli295@wisc.edu</p>
             <p>Phone: 573-808-4918</p>
-            <p><a href="http://www.linkedin.com/in/kevin-li-5698a9221">linkedin</a></p>
+            <p><a href="http://www.linkedin.com/in/kevin-li-5698a9221">Linkedin</a></p>
           </div>
         </article>
     </main>
