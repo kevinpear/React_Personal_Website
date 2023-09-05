06827bc4a3abeaab6cf6a715a64beaaed0583daa
Kevin Li <kli295@wisc.edu>
2023-09-05 13:55:21 -0500

    first completely working version of CSV SQL Query Generator, is slow with large files however

--- src/Custom-Routes/Projects.js
+++ src/Custom-Routes/Projects.js
@@ -1,17 +1,35 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Projects.css'
 import './Routes.css';
 import Papa from 'papaparse';
+const initSqlJs = require('sql.js');
 
 
 const Projects = () => {
- // State variables to handle text box inputs
- const [textbox1Value, setTextbox1Value] = useState('');
- const [generatedQuery, setGeneratedQuery] = useState('');
- const [tableData, setTableData] = useState([]);
 
+ // State variables to handle text box inputs
+ const inital_State = 'Select * From CSV_File;';
+ const [systemContent, setSystemContent] = useState('');
+ const [userQuery, setUserQuery] = useState('');
+ const [tableData, setTableData] = useState([]);
+ let [SqlQuery, setSqlQuery] = useState(inital_State);
+ const [SqlQueryResultHeader, setSqlQueryResultHeader] = useState([]);
+ const [SqlQueryResultData, setSqlQueryResultData] = useState([]);
+ const [db, setDb] = useState(null);
+
+ useEffect(() => {
+   // Initialize the database when the component mounts
+   const initDatabase = async () => {
+     const SQL = await initSqlJs({
+       locateFile: (file) => `https://sql.js.org/dist/${file}`,
+     });
+     const newDb = new SQL.Database();
+     setDb(newDb);
+   };
+
+   initDatabase();
+ }, []);
 
  // Function to handle button click
  const handleButtonClick = async () => {
@@ -21,11 +39,12 @@ const Projects = () => {
      const requestBody = {
        operation: 'gpt_query',
        payload: {
-         system_content: "Given the following SQL tables, your job is to write queries given a user’s request.\n\nCREATE TABLE Orders (\n  OrderID int,\n  CustomerID int,\n  OrderDate datetime,\n  OrderTime varchar(8),\n  PRIMARY KEY (OrderID)\n);\n\nCREATE TABLE OrderDetails (\n  OrderDetailID int,\n  OrderID int,\n  ProductID int,\n  Quantity int,\n  PRIMARY KEY (OrderDetailID)\n);\n\nCREATE TABLE Products (\n  ProductID int,\n  ProductName varchar(50),\n  Category varchar(50),\n  UnitPrice decimal(10, 2),\n  Stock int,\n  PRIMARY KEY (ProductID)\n);\n\nCREATE TABLE Customers (\n  CustomerID int,\n  FirstName varchar(50),\n  LastName varchar(50),\n  Email varchar(100),\n  Phone varchar(20),\n  PRIMARY KEY (CustomerID)\n);",
-         user_query: "Write a SQL query which computes the average total order value for all orders on 2023-04-01.",
+         model: "gpt-3.5-turbo",
+         system_content: systemContent,
+         user_query: userQuery,
        },
      };
-
+     console.log(systemContent);
 
      const response = await axios.post(apiEndpoint, requestBody);
      console.log('Full Response Data:', response.data);
@@ -35,14 +54,14 @@ const Projects = () => {
        .replace(/[^\x20-\x7E\n]/g, '-') // Replace non-printable characters except \n with '-'
        .replace(/(["'\\])/g, '\\$1'); // Escape quotes and backslashes
       
-     setGeneratedQuery(sanitizedData);
+     setSqlQuery(sanitizedData);
+     SqlQuery = sanitizedData;
+     Generate_SQL_Queried_Table();
    } catch (error) {
      console.error('Error occurred:', error);
-     setGeneratedQuery('Error occurred while making the request.');
+     setSqlQuery('Error occurred while making the request.');
    }
-   //setGeneratedQuery("");
  };
-
 
  const handleFileUpload = (e) => {
    const file = e.target.files[0];
@@ -50,23 +69,86 @@ const Projects = () => {
      const reader = new FileReader();
      reader.onload = (event) => {
        const csvData = event.target.result;
-
-
+      
+       // Get file data
        Papa.parse(csvData, {
          header: true,
          skipEmptyLines: true,
          dynamicTyping: true,
          complete: (result) => {
            setTableData(result.data);
+           if (result.data.length > 0) {
+            // generate system_content part of Open_AI API Query
+            const header = Object.keys(result.data[0]).join(', ').replace(/ /g, '_');
+            const firstRow = Object.values(result.data[0])
+              .map((value) => `"${value}"`)
+              .join(', ');
+            
+            const systemContentString = `You are a SQL query generator for a CSV file that is converted to a database named CSV_File. Only generate the query and nothing else. The header of the CSV file is "${header}" and the first row is "${firstRow}".`;
+            setSystemContent(systemContentString);
+
+            // Setup database based on file data
+            const values = result.data.map((row) =>
+            Object.values(row).map((value) =>
+              typeof value === 'string' ? `'${value}'` : value
+              )
+            );
+            //console.log([header, firstRow, values, Generate_Insert_SQL(values)]);
+            db.run(`DROP TABLE IF EXISTS csv_data_original`);
+            db.run(`CREATE TABLE csv_data_original (${header})`);
+            db.run( Generate_Insert_SQL(values));
+            console.log([db.exec("SELECT * FROM csv_data_original"),"table"]);
+            Generate_SQL_Queried_Table();
+          }
          },
        });
       };
      reader.readAsText(file);
+     setSqlQuery(inital_State);
    }
  };
 
+ function Generate_Insert_SQL(values){
+  var String_total = "INSERT INTO csv_data_original VALUES";
+  for (let row = 0; row < values.length; row++) {
+    String_total += " (";
+    for (let col = 0; col < values[row].length - 1; col++) {
+      var value = values[row][col] + "";
+      if (value != null){
+        value = value.replace(/(?<=.)'(?=.)/g,  '"');
+      }
+      String_total += value + ", ";
+    }
+    value = values[row][values[row].length-1] + "";
+      if (value != null){
+        value = value.replace(/(?<=.)'(?=.)/g,  '"');
+      }
+    String_total += value + "),\n";
+  }
+  return String_total.slice(0, -2) + ";";
+ }
 
-
+ function Generate_SQL_Queried_Table(){
+  console.log(SqlQuery);
+  try {
+    const dropTableSQL = `DROP TABLE IF EXISTS CSV_File`;
+    const copyTableSQL = `CREATE TABLE IF NOT EXISTS CSV_File AS SELECT * FROM csv_data_original`;
+    db.run(dropTableSQL);
+    db.run(copyTableSQL);
+    const result = db.exec(SqlQuery);
+    console.log(result);
+    if (result && result.length > 0) {
+      setSqlQueryResultHeader(result[0].columns);
+      setSqlQueryResultData(result[0].values);
+    } else {
+      //'No results found.'
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    //'Error occurred while executing the query.'
+  }
+ }
+ 
 
  return (
    <main>
@@ -75,12 +157,15 @@ const Projects = () => {
          <h1 className="introduction">Projects</h1>
        </header>
        <div className="route-content">
+
          <h2 className="Title">CSV SQL Query Generator:</h2>
+
          <input
            type="file"
            accept=".csv"
            onChange={handleFileUpload}
          />
+
          <div id="csv-table-container">
            <table className="csv-table">
              <thead>
@@ -104,31 +189,55 @@ const Projects = () => {
              </tbody>
            </table>
          </div>
+
          <br></br>
+
          <div>
            <textarea
              className='csv-gpt-prompt'
              type="text"
-             value={textbox1Value}
-             onChange={(e) => setTextbox1Value(e.target.value)}
+             value={userQuery}
+             onChange={(e) => setUserQuery(e.target.value)}
              placeholder="What do you want to know from the CSV?"
            />
          </div>
-         <button className='generate-query-button' onClick={handleButtonClick}>Generate</button>
-         <p className='query'>
-           <strong>Query: </strong>{generatedQuery}
-           <br></br>
-           <textarea className='csv-query-query' name="myTextArea" id="myTextArea" type="text">
-           Select * From *;
-           </textarea>
-         </p>
+
+         <div>
+          <button className='generate-query-button' onClick={handleButtonClick}>Generate</button>
+         </div>
+
+         <textarea className='csv-query-query' name="myTextArea" id="myTextArea" type="text" value={SqlQuery} onChange={(e) => setSqlQuery(e.target.value)}>
+         </textarea>
+        
+         <div>
+          <button className='run-query-button' onClick={ Generate_SQL_Queried_Table}>Run Query</button>
+         </div>
+
+         <div id="csv-table-container">
+           <table className="csv-table">
+            <thead>
+              <tr>
+                {SqlQueryResultHeader.map((column, columnIndex) => (
+                  <th key={columnIndex}>{column}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {SqlQueryResultData.map((row, rowIndex) => (
+                <tr key={rowIndex} className="csv-table-row">
+                  {row.map((cell, cellIndex) => (
+                    <td key={cellIndex} className="csv-table-cell">{cell}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
        </div>
      </article>
    </main>
  );
 };
 
-
 export default Projects;
-
-
